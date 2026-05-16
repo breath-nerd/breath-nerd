dd58cf4fb01add50b24fa067706303dbd68177f7
Kanami Anderson <kanamianderson@gmail.com>
2026-05-16 11:01:48 -0700

    feat(favcon): add new image fix(node_modules): relocate folder for vite to access and restore front end server connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import type { User } from "./types";
-// import LoginModal from "./components/LoginModal";
+import LoginModal from "./components/LoginModal";
 import BreathingPage from "./components/BreathingPage";
 
 /*
@@ -68,7 +68,7 @@ function App() {
   return (
     <>
       <BreathingPage user={user} onLogout={handleLogout} isBlurred={!user} />
-      {/* {!user && <LoginModal onLoginSuccess={(user: User) => setUser(user)} />} */}
+      {!user && <LoginModal onLoginSuccess={(user: User) => setUser(user)} />}
     </>
   );
 }
